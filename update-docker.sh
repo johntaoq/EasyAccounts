1de83043c983f1ed539815f5e31fa9601feaa96e
ubuntu <ubuntu@deep-blue.cloud>
2026-01-26 14:47:48 +0800

    feat: v2.6.1 优化内存、新增页面恢复数据、系统公告功能

--- update-docker.sh
+++ update-docker.sh
@@ -1,5 +1,5 @@
 docker pull 775495797/easyaccounts-mysql:1.0.0 &&
-docker pull 775495797/easyaccounts-nginx:4.0.0 &&
-docker pull 775495797/easyaccounts-server:2.5.1 &&
+docker pull 775495797/easyaccounts-nginx:4.0.1 &&
+docker pull 775495797/easyaccounts-server:2.6.1 &&
 docker pull 775495797/easyaccounts-webhook:1.0.0 &&
 docker pull 775495797/easyaccounts-ai:1.1.0
